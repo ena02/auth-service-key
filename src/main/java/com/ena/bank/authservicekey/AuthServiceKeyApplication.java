72e1a7ef01c6eb6e672e40e48afb54dca42efaa1
ena <erkembekov.nurzhan@mail.ru>
2024-03-28 03:55:18 +0500

    Initial commit

---
+++ src/main/java/com/ena/bank/authservicekey/AuthServiceKeyApplication.java
@@ -0,0 +1,13 @@
+package com.ena.bank.authservicekey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceKeyApplication.class, args);
+    }
+
+}
